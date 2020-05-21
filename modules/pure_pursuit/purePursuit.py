1dcd22a56cd02794adf00403a4f212d9db8ac90a
sharongoldi <sharon19g@gmail.com>
2020-05-21 14:38:08 +0300

    DONE: added an ugly patch in PP for cases when we dont have a real solution in calc_look_ahead_point

--- modules/pure_pursuit/purePursuit.py
+++ modules/pure_pursuit/purePursuit.py
@@ -43,6 +43,8 @@ class PurePursuitController:
     def calculate_steering(self): # calc the needed steering angle to course correct to the next waypoint
         ld = max(self.velocity * self._kdd, 2)
         look_ahead_point = self._calculate_look_ahead_point(ld)
+        # look_ahead_point.sort()
+        # look_ahead_point = sorted(look_ahead_point)
 
         alpha = math.atan2(look_ahead_point[1] - self.coordinates[1], look_ahead_point[0] - self.coordinates[0]) - self.orientation # error angle
         delta = math.atan2(2*self._car_length*math.sin(alpha), ld)
@@ -51,9 +53,11 @@ class PurePursuitController:
     def _calculate_look_ahead_point(self, ld):
         x_vec = solve((self.path[3]*x**3 + self.path[2]*x**2 + self.path[1]*x + self.path[0] - self.coordinates[1])**2 + (x - self.coordinates[0])**2 - ld**2, x)
         for p in x_vec:
-            if p.is_real:
-                if p > self.coordinates[0]:
-                    return [float(p), float(self.path(float(p)))]
+            if p.is_real and p > self.coordinates[0]:
+                return p, self.path(p)
+
+        # TODO: this is an ugly patch for cases when we don't have a real solution to the equation (all are complex)
+        return 2, self.coordinates[1]
 
 
 
