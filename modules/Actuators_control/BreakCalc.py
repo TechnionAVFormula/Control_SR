93bf6cf68ebe0b0543e3c44b14afc39a37ec8bd8
Adi-Levy <adilevy15@gmail.com>
2020-07-28 14:17:31 +0300

    added shells for actuator percentage calculations

---
+++ modules/Actuators_control/BreakCalc.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+class BreakCalc: #TODO: add calc model
+    def __init__(self):
+
+    def optimal_break_percent(self, curr_speed, req_speed):
+
+        return req_break_percent
